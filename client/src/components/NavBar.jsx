a1e603ea4ac95a196b9cd18180c674274b7adb56
Aashish Chadha - Hornbill JOBS <punya.tkc@gmail.com>
2024-11-02 01:56:58 +0530

    first commit

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
         <Navbar bg="dark" className="mb-4" style={{height:"3.75rem"}}>
             <Container>
                 <h2>
-                    <Link to="/" className="link-light text-decoration-none">Let Us Chat</Link>
+                    <Link to="/" className="link-light text-decoration-none">Let's Chat</Link>
                 </h2>
                 {user && <span className="text-warning">Logged in as {user.name}</span>}
                 <Nav>
